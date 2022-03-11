34c4a62359e3feb54b7d7e859fedad3fa6ac04a6
cleciosan <cleciosan@hotmail.com>
2022-03-11 17:40:27 -0300

    Inicio Projeto POOConcepts

---
+++ POOConcepts/POOConcepts/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace POOConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POO Concepts!");
+            Console.WriteLine("==============");
+
+        }
+    }
+}
