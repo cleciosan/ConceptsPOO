7568619126590dc0244bc10bed234ed7ad7d536d
cleciosan <cleciosan@hotmail.com>
2022-03-22 16:59:37 -0300

    FInish date class

--- POOConcepts/POOConcepts/Date.cs
+++ POOConcepts/POOConcepts/Date.cs
@@ -20,8 +20,66 @@ namespace POOConcepts
         public Date(int year, int month, int day)
         {
             _year = year;
-            _month = month;
-            _day = day;
+            _month = CheckMonth(month);
+            _day = Checkday(year, month,day);
+        }
+
+        private int Checkday(int year, int month, int day)
+        {
+           if(month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month])
+            {
+                return day;
+            }
+
+            throw new DayException("Invalid day");
+
+
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+            //if (year % 4 == 0)
+            //{
+            //    if(year % 100 == 0)
+            //    {
+            //        if (year % 400 == 0)
+            //        {
+            //            return true;
+            //        }
+
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+            //else
+            //{
+            //    return true;
+            //}
+        }
+
+        private int CheckMonth(int month)
+        {
+           if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+
+            throw new MonthException("Invalid month");
+        }
+
+        public override string ToString()
+        {
+            //return _day +"/"+ _month +"/"+_year;
+            return $"{_day:00}/{_month:00}/{_year}"; // Interpolação
         }
     }
 }
--- POOConcepts/POOConcepts/Program.cs
+++ POOConcepts/POOConcepts/Program.cs
@@ -8,6 +8,20 @@ namespace POOConcepts
         {
             Console.WriteLine("POO Concepts!");
             Console.WriteLine("==============");
+            //Date date = new Date(2022,03,15);
+            //Console.WriteLine(date.ToString());
+
+            try
+            {
+                Console.WriteLine(new Date(2024, 2, 29));
+                Console.WriteLine(new Date(1974, 9, 23));
+                Console.WriteLine(new Date(1985, 13, 30));
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine(error.Message); ;
+            }
 
         }
     }
